1c1c927d80968d48eb097e082a0bf63b68b79a87
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2022-06-08 21:43:33 +0200

    Swagger

--- src/airports/airports.module.ts
+++ src/airports/airports.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common"
+import { AirportsController } from "./airports.controller"
 import { AirportsService } from "./airports.service"
 
 @Module({
-  providers: [AirportsService]
+  providers: [AirportsService],
+  controllers: [AirportsController],
+  exports: [AirportsService]
 })
 export class AirportsModule {}
